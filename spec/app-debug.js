20406e37f416d2710296ac618a7f1761b770e961
Khalid Salomão <khalidsalomao@gmail.com>
2016-05-20 14:57:13 -0300

    Debug for vs code

---
+++ spec/app-debug.js
@@ -0,0 +1,9 @@
+var qs = require('../src/simplequerystring.js');
+
+var str = qs.parse('http://example.org/?key1=val1&key2=100&&&key3&&key4=&&key5=5#anchor');
+console.log(str);
+//=> { key1: 'val1', key2: '100' }
+
+var dic = qs.stringify({ key1: 'val1', key2: 100, key3: true, key4: null, key5: undefined });
+console.log(str);
+//=> 'key1=val1&key2=100&key3=true'
